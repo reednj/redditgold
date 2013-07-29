e5e1c8250bba3cc4dc1ffb38f1e62aaee51bf932
reednj <reednj@gmail.com>
2013-07-29 15:11:21 +0800

    bugfix to deply script

--- config/pr-deploy.sh
+++ config/pr-deploy.sh
@@ -4,7 +4,7 @@ BASE=~/gold.popacular.com
 
 # copy the required files to the website
 rm -rf $BASE/*
-cp -R ~/code/redditgold.git/* $BASE/*
+cp -R ~/code/redditgold.git/* $BASE
 rm -rf $BASE/sh
 cp ~/code/config_backup/redditgold/esql.dbpass.php $BASE/config/
 
