a2ca3e50e23a67d1b5bf6f5acc63e6758e4a0fb7
reednj <reednj@gmail.com>
2013-08-01 13:49:59 +0800

    chart added to the page
    Still many problems with it though

--- index.php
+++ index.php
@@ -19,9 +19,65 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title> reddit gold </title>
+<title>gold-counter - tracking revenue from reddit gold</title>
 
 <link rel="stylesheet" href="content/css/base.css" type="text/css" media="screen, projection">
+
+<script type='text/javascript' src='content/js/Chart.min.js'></script>
+<script type='text/javascript'>
+
+_weekly_data = <?= json_encode($weekly_data) ?>;
+
+window.addEvent('load', function() {
+	var labels = [];
+	var data = [];
+	var week_count = 26;
+	for(var i=0; i < week_count; i++) {
+		if(week_count - i - 1 < _weekly_data.length) {
+			var week = _weekly_data[week_count - i - 1];
+			data.push(week.revenue);
+		} else {
+			data.push(0);
+		}
+
+		if(i % 4 == 0) {
+			labels.push('Jan');
+		} else {
+			labels.push('');
+		}
+	}
+
+	var ctx = $('weekly-chart').getContext('2d');
+	var chart = new Chart(ctx).Bar({
+		labels: labels,
+		datasets: [
+			{
+				fillColor : "rgba(220,100,100,0.8)",
+				strokeColor : "rgba(220,100,100,1)",
+				data: data
+			}
+		]
+	},
+	{
+		scaleStartValue: 0,
+		barValueSpacing: 1,
+		barStrokeWidth: 1
+	});
+});
+
+function addEvent(event, fn) {
+    if (window.addEventListener) {
+        window.addEventListener(event, fn, false);
+    } else if (window.attachEvent) {
+        window.attachEvent('on' + event, fn);
+    }
+}
+
+function $(id) {
+	return document.getElementById(id);
+}
+
+</script>
 
 </head>
 
@@ -50,7 +106,7 @@
 			<div class='so-label'> last week <span class='so-rate'> ($<?= number_format($week_rate, 2); ?> / hr)</span> </div>
 		</div>
 
-		<div class='so-counter-block'>
+		<div class='so-counter-block last'>
 			<div class='so-counter'> $<?= number_format($day_count); ?> </div>
 			<div class='so-label'> last 24 hrs <span class='so-rate'> ($<?= number_format($day_rate, 2); ?> / hr)</span> </div>
 		</div>
@@ -61,31 +117,33 @@
 
 	<div class='clear'></div>
 
-	<div class='block'>
+	<canvas id="weekly-chart" width="605" height="220">
+		<div class='block'>
 
-		<table>
-			<thead>
+			<table>
+				<thead>
+					<tr>
+						<td colspan='2'><h2>Revenue by Week</h2></td>
+					</tr>
+				</thead>
+
+				<tbody>
+				<?php foreach($weekly_data as $week) { ?>
+
 				<tr>
-					<td colspan='2'><h2>Revenue by Week</h2></td>
+					<td><?= $week['year'].'-'.$week['week'] ?></td>
+					<td>$<?= number_format($week['revenue']) ?></td>
 				</tr>
-			</thead>
 
-			<tbody>
-			<?php foreach($weekly_data as $week) { ?>
-
-			<tr>
-				<td><?= $week['year'].'-'.$week['week'] ?></td>
-				<td>$<?= number_format($week['revenue']) ?></td>
-			</tr>
-
-			<?php } ?>
-			</tbody>
-		</table>
-	</div>
+				<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</canvas>
 
 	<div class='clear'></div>
 
-	<div class='block'>
+	<div class='block last'>
 
 		<table>
 			<thead>
