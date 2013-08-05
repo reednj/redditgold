b4bee980809841dd5ec5824fbc9ca9947b263aaa
reednj <reednj@gmail.com>
2013-08-05 18:00:57 +0800

    have the deploy script echo some information

--- config/pr-deploy.sh
+++ config/pr-deploy.sh
@@ -11,8 +11,15 @@ cp -R $SRC/* $WEB
 rm -rf $WEB/sh
 cp $CONFIG/db.rb $WEB/config/
 
+mkdir $WEB/tmp
+touch $WEB/tmp/restart.txt
+
+echo "Website deployed"
+
 # now to the script folder
 rm -rf $SCRIPTS/*
 mkdir $SCRIPTS/sh
 cp -R $SRC/sh/* $SCRIPTS/sh
 cp $CONFIG/esql.dbpass.php $SCRIPTS/sh/lib
+
+echo "Script deployed"
