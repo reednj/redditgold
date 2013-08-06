80ac4eb9aca4c75ccd1d243f7a36b268505c6b80
reednj <reednj@gmail.com>
2013-08-06 12:03:03 +0800

    update deploy script to send to the reddit-stream dir

--- config/pr-deploy.sh
+++ config/pr-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+# try using this command to deploy the script more easily:
+# scp config/pr-deploy.sh reednj@servralert.com:~/code/`basename "$PWD"`.git/.git/hooks/pr-deploy
 
-WEB=~/gold.popacular.com
+WEB=~/gold.reddit-stream.com
 SRC=~/code/redditgold.git
 SCRIPTS=~/scripts/redditgold
 CONFIG=~/code/config_backup/redditgold
