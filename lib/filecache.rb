51a43316ddccf08ce42f83ce6cf859d9875726c2
reednj <reednj@gmail.com>
2013-08-07 10:59:08 +0800

    some changes to the filecache to make it more standard

--- lib/filecache.rb
+++ lib/filecache.rb
@@ -1,4 +1,10 @@
-
+# FileCache
+#
+# Caches an object on the file system in json format.
+#
+# By default stores the data in ./cache. Pass a different path to the constructor to
+# change this
+#
 class FileCache
 	def initialize(dir='cache')
 		@dir=dir
@@ -8,15 +14,18 @@ class FileCache
 		end
 	end
 
+	# Cache the object produced by the passed block in +filename+
+	# If the file already exists, and the mtime is less than the max
+	# age, then the file will be parsed and returned
 	def cache(filename, maxage_sec)
-		data = self.fromCache(filename, maxage_sec)
+		data = self.from_cache(filename, maxage_sec)
 		
 		if(data != nil)
 			return data
 		else
 			data = yield
 
-			File.open(self.getPath(filename), 'w') { |file| 
+			File.open(self.get_path(filename), 'w') { |file| 
 				# we need to wrap the data in an object, or the parsing can
 				# fuck up for simple objects like strings, floats etc
 				file.write({:data => data}.to_json) 
@@ -29,8 +38,8 @@ class FileCache
 
 	# checks the cache, and returns the file if it exits, and has not expired, otherwise
 	# just returns nil
-	def fromCache(filename, maxage_sec)
-		path = self.getPath filename
+	def from_cache(filename, maxage_sec)
+		path = self.get_path filename
 		if File.exist?(path) && File.mtime(path) > Time.now - maxage_sec
 			json_obj = JSON.parse(File.read(path), {:symbolize_names => true})
 			return json_obj[:data]
@@ -39,7 +48,7 @@ class FileCache
 		end
 	end
 
-	def getPath(filename)
+	def get_path(filename)
 		return File.join(@dir, filename)
 	end
 
