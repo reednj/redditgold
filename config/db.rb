d3b54789746f0ff6f085bbe4f138277005e2cbaa
Nathan Reed <reednj@gmail.com>
2015-06-22 09:26:32 +0800

    db needs to use IP addr in development, to force it to use a TCP socket, instead of a filesystem one

--- config/db.rb
+++ config/db.rb
@@ -3,7 +3,7 @@ AppConfig = OpenStruct.new({
 	:db => OpenStruct.new({
 		:username => 'linkuser',
 		:password => '',
-		:host => 'localhost',
+		:host => '127.0.0.1',
 		:database => 'gold'
 	})
 })
