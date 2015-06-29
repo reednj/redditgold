92b941bf8a08045687c4e42e06a42f53358283ce
Nathan Reed <reednj@gmail.com>
2015-06-29 09:59:58 +0800

    basic template for adding comments. Have added php code to port

--- sh/gold.rb
+++ sh/gold.rb
@@ -2,6 +2,9 @@
 
 require 'sequel'
 require 'yaml'
+require 'json'
+require 'rest-client'
+
 require '../config/db.rb'
 require '../lib/simpledb'
 
@@ -12,7 +15,71 @@ class App
 	end
 
 	def main
-		puts @db.db[:comments].first.to_yaml
+		response = reddit_data
+		comments = remove_duplicates response
+
+		insert_comments comments
+	end
+
+	def reddit_data
+		data = RestClient.get 'http://www.reddit.com/r/all/comments/gilded.json?count=50'
+		return JSON.parse(data, :symbolize_names => true)
+	end
+
+	def remove_duplicates(response)
+		#foreach($data->data->children as $comment) {
+		#	if($comment->data->id == $last_comment) {
+		#		break;
+		#	}
+		#
+		#	array_push($new_comments, $comment);
+		#}
+
+		response[:data][:children]
+	end
+
+	def insert_comments(comments)
+		#foreach(array_reverse($new_comments) as $comment) {
+		#	
+		#	print  $comment->data->id . "\n";
+		#
+		#	if(is_thread($comment->data->name)) {
+		#		$thread_id = $comment->data->name;
+		#		$title = $comment->data->title;
+		#		$body = $comment->data->is_self == 1 ? $comment->data->selftext : '';
+		#	} else {
+		#		$thread_id = $comment->data->link_id;
+		#		$title = $comment->data->link_title;
+		#		$body = $comment->data->body;
+		#	}
+		#
+		#	Comments::Insert(array(
+		#		'comment_id' => $comment->data->id,
+		#		'thread_id' => $thread_id,
+		#		'user' => $comment->data->author,
+		#		'subreddit' => $comment->data->subreddit,
+		#		'gold_count' => $comment->data->gilded,
+		#		'post_date' => date('c', $comment->data->created_utc)
+		#	));
+		#	
+		#	ESQL::Insert('comment_content', array(
+		#		'comment_id' => $comment->data->id,
+		#		'content' => $body,
+		#		'title' => $title
+		#	));
+
+		comments.each do |comment|
+			puts "#{comment[:data][:id]} (is_thread: #{thread? comment})"
+		end
+
+	end
+
+	def thread?(comment)
+		comment[:data][:name].start_with? 't3_'
+	end
+
+	def last_comment_id
+		@db.db[:comments].reverse_order(:gold_id).get :comment_id
 	end
 end
 
