f0699d55c56eaa918d0037658a4d6ef09aa5698c
Nathan Reed <reednj@gmail.com>
2016-05-13 12:54:16 +0800

    extend the twitter config in a nice way

--- sh/tweet.rb
+++ sh/tweet.rb
@@ -104,6 +104,34 @@ class CommentContent < Sequel::Model(:comment_content)
 	end
 end
 
+module Twitter::Config
+	class YAMLConfig
+		def self.for_user(username, options = {})
+			path = _twitter_config_file options[:path]
+			config_data = YAML.load_file path
+			user_config = config_data[username]
+			raise "no config found for #{username} in file #{path}" if user_config.nil?
+
+			return user_config
+		end
+
+		def self._twitter_config_file(path = nil)
+			result = [path, '~/.twitter.yaml', './twitter.yaml'].compact.select{ |f| File.exist? f }.first
+			raise 'could not find twitter config' if result.nil?
+			return result
+		end
+	end
+end
+
+module Twitter::REST
+	class Client
+		def self.from_config(username, options = {})
+			self.new Twitter::Config::YAMLConfig.for_user username, options
+
+		end
+	end
+end
+
 class App
 
 	def main
@@ -117,8 +145,9 @@ class App
 		# tweet the thread / comment, and print it to the 
 		# console
 		tweet_content = comment.to_tweet
-		twitter_client.update tweet_content 
 		puts tweet_content
+		twitter_client.update tweet_content 
+		
 		
 		# now mark the comment as tweeted so we won't send it out again
 		comment.was_tweeted = 1
@@ -134,24 +163,12 @@ class App
 	def twitter_client
 		username = 'reednj'
 		config_file = '../config/twitter.yaml'
-		@twitter_client ||= Twitter::REST::Client.new twitter_config(username, :path => config_file)
+		@twitter_client ||= Twitter::REST::Client.from_config(username, :path => config_file)
 	end
 
-	def twitter_config(username, options = {})
-		path = twitter_config_file options[:path]
-		config_data = YAML.load_file path
-		user_config = config_data[username]
-		raise "no config found for #{username} in file #{path}" if user_config.nil?
 
-		return user_config
-	end
-
-	def twitter_config_file(path = nil)
-		result = [path, '~/.twitter.yaml', './twitter.yaml'].compact.select{ |f| File.exist? f }.first
-		raise 'could not find twitter config' if result.nil?
-		return result
-	end
 end
 
 
 App.new.main
+
