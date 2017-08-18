f60a5f0eaa26b2ba955f08f51cdf0946e9cc97f1
Nathan Reed <reednj@gmail.com>
2017-08-18 15:00:16 +0800

    use abs path for prod config

--- config/db.rb
+++ config/db.rb
@@ -2,6 +2,6 @@ require './config/yaml-load_first_file'
 
 AppConfig = YAML.load_first_file([
 	"#{ENV['HOME']}/config/gold.db.conf",
-	"#{ENV['HOME']}/code/config_backup/gold.db.conf",
+	"/home/reednj/code/config_backup/gold.db.conf",
 	"./config/dev.config.yaml"
 ])
