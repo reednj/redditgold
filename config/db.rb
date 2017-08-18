dd00a125b54b9bc09ad6c344a75a2104d5116a80
Nathan Reed <reednj@gmail.com>
2017-08-18 14:51:17 +0800

    look for dreamhost config file

--- config/db.rb
+++ config/db.rb
@@ -2,5 +2,6 @@ require './config/yaml-load_first_file'
 
 AppConfig = YAML.load_first_file([
 	"#{ENV['HOME']}/config/gold.db.conf",
+	"#{ENV['HOME']}/code/config_backup/gold.db.conf",
 	"./config/dev.config.yaml"
 ])
